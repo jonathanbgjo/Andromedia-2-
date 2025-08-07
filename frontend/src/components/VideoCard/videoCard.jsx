e7acb5e9f8714a8457e4702936a8f9fd9da2ffeb
Jonathan Jo <jonathanbgjo@gmail.com>
2025-08-06 23:14:29 -0400

    setup home page, base side/nav bar

---
+++ frontend/src/components/VideoCard/videoCard.jsx
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+import styles from './VideoCard.module.css';
+export default function VideoCard({video}){
+    return (
+        <Link to={`/watch/${video.id}`} className={styles.link}>
+            <div>
+                <img src="../IMG_9795" alt="" />
+                <div>
+                    <h3>{video.title}</h3>
+                    <p>{video.channelName}</p>
+                    <p>{video.views}</p>
+                </div>
+            </div>
+        </Link>
+    )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,36 @@
+import VideoCard from "../components/VideoCard/videoCard";
+import {useState, useEffect} from 'react'
 //DISPLAYS LIST OF VIDEOCARDS (video previews)
+export default function Home(){
+    
+    const [videos, setVideos] = useState([]);
+    const tempVideos = [
+        {id: "1",
+        channelName: "testOne",
+        views: "44"},
+        {id: "2",
+        channelName: "testTwo",
+        views: "1234"},
+        {id: "3",
+        channelName: "testTree",
+        views: "565"},
+        {id: "5",
+        channelName: "testFour",
+        views: "13341"},
+    ]
+    useEffect(() => {
+        setVideos(tempVideos);
+    }
+
+    )
+    return (
+        <div>
+            <h1>Testing Home Page</h1>
+            <div>
+                {videos.map((video)=> (
+                    <VideoCard key={video.id} video={video}/>
+                ))}
+            </div>
+        </div>
+    )
+}
